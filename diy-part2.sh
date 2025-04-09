41a88a8ee65ff737f41f1ee2539395ada1a7b6ce
ymk88877 <951688403@qq.com>
2025-04-09 09:40:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 ./scripts/feeds uninstall luci-app-smartdns smartdns
 ./scripts/feeds install -a -p customsd
 
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
